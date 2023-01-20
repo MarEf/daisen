0cb95d1a603244e2212b12ef264c6417f14ba80c
MarEf <maria.efimova93@gmail.com>
2023-01-20 15:56:43 +0200

    liittymislomakkeet

---
+++ app/main/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, Optional, Regexp
+
+class SignupFormEng(FlaskForm):
+    name = StringField('Full name *', validators=[DataRequired(), Length(max=255)])
+    city = StringField('Place of residence *', validators=[DataRequired(), Length(max=255)])
+    email = StringField('Email *', validators=[DataRequired(), Length(max=254), Email()])
+    hyy = BooleanField('HYY member')
+    studentNumber = StringField('Student Number', validators=[Length(max=10), Regexp('^[0-9]*$'), Optional()])
+    nickname = StringField('Nick (e.g. on IRC or Discord', validators=[Length(max=255), Optional()])
+    discovered = TextAreaField('How did you hear about us?', validators=[Length(max=4000), Optional()])
+    submit = SubmitField('Join')
+
+class SignupFormFi(FlaskForm):
+    name = StringField('Koko nimi *', validators=[DataRequired(), Length(max=255)])
+    city = StringField('Asuinkunta *', validators=[DataRequired(), Length(max=255)])
+    email = StringField('Sähköposti *', validators=[DataRequired(), Length(max=254), Email()])
+    hyy = BooleanField('HYY:n jäsen')
+    studentNumber = StringField('Opiskelijanumero', validators=[Length(max=10), Regexp('^[0-9]*$'), Optional()])
+    nickname = StringField('Nick (esim. IRC tai Discord', validators=[Length(max=255), Optional()])
+    discovered = TextAreaField('Kuinka kuulit meistä?', validators=[Length(max=4000), Optional()])
+    submit = SubmitField('Liity')
--- daisen.py
+++ daisen.py
@@ -5,7 +5,7 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 
 from flask_migrate import Migrate, upgrade
 
@@ -14,6 +14,9 @@ from app.models import User, Member
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+
+from app.main.forms import SignupFormEng, SignupFormFi
 
 @app.cli.command()
 def deploy():
@@ -38,3 +41,25 @@ def users():
 @app.route('/users/<name>')
 def user(name):
     return render_template("user.html", id=id)
+
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+    form = SignupFormEng()
+    if form.validate_on_submit():
+        member = Member(name=form.name.data, city=form.city.data, email=form.email.data, hyy=form.hyy.data, studentNumber=form.studentNumber.data, nickname=form.nickname.data, discovered=form.discovered.data)
+        db.session.add(member)
+        db.session.commit()
+        flash('Welcome to YAMA!')
+        return redirect('/join')
+    return render_template('signup.html', form=form, lang='en')
+
+@app.route('/liity', methods=['GET', 'POST'])
+def liity():
+    form = SignupFormFi()
+    if form.validate_on_submit():
+        member = Member(name=form.name.data, city=form.city.data, email=form.email.data, hyy=form.hyy.data, studentNumber=form.studentNumber.data, nickname=form.nickname.data, discovered=form.discovered.data)
+        db.session.add(member)
+        db.session.commit()
+        flash('Tervetuloa YAMAan!')
+        return redirect('/join')
+    return render_template('signup.html', form=form, lang='fi')
